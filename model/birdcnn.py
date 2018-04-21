af820a6d670c31e5e4cd9c204c0c919f1afc1494
Ian Yake <iangyake@gmail.com>
2018-04-21 15:37:32 -0500

    less overfitting on model

--- model/birdcnn.py
+++ model/birdcnn.py
@@ -46,34 +46,34 @@ train_datagen = ImageDataGenerator(
 test_datagen = ImageDataGenerator(rescale=1./255)
 
 training_set = train_datagen.flow_from_directory(
-        'dataset2/train_set',
+        'dataset3/train_set',
         target_size=(64, 64),
         batch_size=32,
         class_mode='binary') #for the dependent variable
 
 test_set = test_datagen.flow_from_directory(
-        'dataset2/test_set',
+        'dataset3/test_set',
         target_size=(64, 64),
         batch_size=32,
         class_mode='binary')
 
 classifier.fit_generator(
         training_set,
-        steps_per_epoch=358, #number of images in training set
-        epochs=2,
+        steps_per_epoch=310, #number of images in training set
+        epochs=25,
         validation_data=test_set,
-        validation_steps=358, #number of images in test set
+        validation_steps=48, #number of images in test set
         workers = 12,
         max_queue_size = 100) 
     
 
-classifier.save('bird_modelOF.h5')
+classifier.save('bird_model.h5')
 
-
-''' #Making new predictions
+'''
+#Making new predictions
 import numpy as np
 from keras.preprocessing import image
-test_image = image.load_img('dataset2/single_prediction/albatross2.jpg', target_size = (64, 64)) #second argument is target size
+test_image = image.load_img('dataset2/single_prediction/Rufous_Hummingbird_0016_59660.jpg', target_size = (64, 64)) #second argument is target size
 test_image = image.img_to_array(test_image) #allows the model to see the pic with colors/3 dimensions
 test_image = np.expand_dims(test_image, axis = 0) #add extra dimension on image specifying batch number so keras doesn't complain
 result = classifier.predict(test_image)
@@ -83,4 +83,5 @@ if result[0][0] == 1:
 else:
     prediction = 'albatross'
     
-print(prediction)'''
+print(prediction)
+'''
